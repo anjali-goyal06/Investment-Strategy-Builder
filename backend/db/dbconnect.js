5945c270a895b6b713f433747d8be127b09d4162
Anjali <anjali170601@gmail.com>
2022-03-19 01:38:53 +0530

    aws cnnection added

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -1,12 +1,22 @@
 const { sanitize } = require('express-validator');
 var mysql = require('promise-mysql');
 
+/*
 var config = {
   host: "localhost",
   user: "root",
   password: "password",
   port : "3306",
   database : "investmentstrategybuilder"
+}
+*/
+
+var config = {
+  host: "database-1.ctvblfobct2i.ap-south-1.rds.amazonaws.com",
+  user: "admin",
+  password: "strategy",
+  port : "3306",
+  database : "InvestmentStrategyBuilder"
 }
 
 const getDbConnection = async () => {
