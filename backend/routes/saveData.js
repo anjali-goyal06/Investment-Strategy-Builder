872dd0fea5fd43d4f6f5bd263b4af65bb70fb877
MokshikaMitra <mokshika06135@gmail.com>
2022-03-23 20:19:32 +0530

    Added Save Route and MakePlot Route

--- backend/routes/saveData.js
+++ backend/routes/saveData.js
@@ -34,29 +34,6 @@ const InstrumentManager = require('../Model/InstrumentManager');
     ]
 }
 */
-
-router.post('/MakePlot', async(req, res)=>{
-
-  console.log(req.body);
-    var investmentStrategy = await new InvestmentStrategy(-1, req.body.StockName, req.body.Ticker, -1, req.body.ExpiryDate, req.body.Name, -1, req.body.Description);
-    var instrumentManager = await new InstrumentManager();
-    //console.log(req.body.instruments);
-    for(var i=0; i<req.body.listInstruments.length; i++){
-      var instrument = req.body.listInstruments[i];
-      var _instrument = await instrumentManager.createInstrument(instrument.segment, instrument.Quantity, instrument.StrikePrice, instrument.Price, instrument.Type, instrument.Side);
-      console.log(investmentStrategy.instruments)
-      investmentStrategy.instruments.push(_instrument);
-    }
-  
-    var plot = await investmentStrategy.combinedPlot();
-    // = await investmentStrategy.getPlot();
-    var response = plot
-    console.log(response)
-    res.send(response);
-  
-  });
-
-
   
 
 router.post('/SaveStrategy' , async (req,res)=>{
